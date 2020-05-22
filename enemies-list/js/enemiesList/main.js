eac4fdf404abfa403eeedbb703f4aa263e307883
Jayson Rice <jayson.rice.95@gmail.com>
2020-05-22 17:03:33 -0400

    Started on enemies list.

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,6 +1,22 @@
-function main() {
-  // Put your code here
-
+const CreateEnemy = (FirstName, LastName, IsHated, Offenses) => {
+  return {
+    FirstName,
+    LastName,
+    IsHated,
+    Offenses,
+  }
 }
 
+function main() {
+  // Put your code here
+  console.log("My Enemies List!")
+  console.log("----------------")
+
+
+
+  let enemies = []
+  let enemy1 = CreateEnemy("Joshua", "Flowers", true, "Being a jerk to me in elementary school")
+  enemies.push(enemy1)
+  console.table(enemies)
+}
 main();
