12a7380854420acafce554c289182d21e6ddc934
Jayson Rice <jayson.rice.95@gmail.com>
2020-05-26 14:33:38 -0400

    Enemies list translated to JS.

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -16,7 +16,18 @@ function main() {
 
   let enemies = []
   let enemy1 = CreateEnemy("Joshua", "Flowers", true, "Being a jerk to me in elementary school")
-  enemies.push(enemy1)
-  console.table(enemies)
+  let enemy2 = CreateEnemy("Darth", "Vader", false, "Cut off Luke's hand")
+  let enemy3 = CreateEnemy("Betty", "Rudelady", true, "Phone calls in the theater")
+  let enemy4 = CreateEnemy("Leon", "Peck", false, "Keeps giving me a hotplate")
+  enemies.push(enemy1, enemy2, enemy3, enemy4)
+
+  enemies.forEach(enemy => {
+    if (enemy.IsHated === true) {
+      console.log(`${enemy.FirstName} ${enemy.LastName} (Really, really dislike!)`)
+    } else {
+      console.log(`${enemy.FirstName} ${enemy.LastName}`)
+    }
+  });
+
 }
 main();
